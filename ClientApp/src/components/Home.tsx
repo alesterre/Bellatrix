7521bef2cf4c1eac633fccfde9d0a08911e5ab94
Sergey Alekseev <alesterre@gmail.com>
2020-05-25 00:42:03 +0400

    Unnecessary files deleted, everything else is converted to TS

---
+++ ClientApp/src/components/Home.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = 'Orders';
+
+  render () {
+    return (
+      <div>
+        <h1>Orders</h1>
+        <p>There will be orders</p>
+      </div>
+    );
+  }
+}
--- ClientApp/src/components/NavMenu.tsx
+++ ClientApp/src/components/NavMenu.tsx
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
-export class NavMenu extends Component {
+interface IProps {
+}
+
+interface IState {
+  collapsed: boolean;
+}
+
+export class NavMenu extends Component<IProps, IState> {
   static displayName = NavMenu.name;
 
-  constructor (props) {
+  constructor (props: IProps) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
